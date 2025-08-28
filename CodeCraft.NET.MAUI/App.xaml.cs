5e9d008e8367f74efd2d40bccd45be8299d99307
Abraham Orihuela Mayor <a.orihuela.mayor@gmail.com>
2025-08-28 13:18:18 +0200

    Initialize .NET MAUI project with cross-platform support
    Updated the project to use .NET MAUI, adding essential files such as `App.xaml`, `AppShell.xaml`, and their code-behind files. Modified the project file to target multiple frameworks (Android, iOS, Mac Catalyst, Windows) and included new resource files for colors and styles. Created `MainPage.xaml` for the main UI, and updated platform-specific files like `AndroidManifest.xml` and `Info.plist` to support the new structure. Added binary files for fonts and images, along with splash and app icon assets. Included privacy compliance and Tizen support files, ensuring a comprehensive setup for cross-platform development.

---
+++ CodeCraft.NET.MAUI/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeCraft.NET.MAUI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			return new Window(new AppShell());
+		}
+	}
+}
---
+++ CodeCraft.NET.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CodeCraft.NET.MAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
